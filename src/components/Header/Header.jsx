b9ef8273b8841bb62e9b78a88877c99a3bade9de
Hisham6667 <hisham.44678@gmail.com>
2023-05-03 18:15:35 +0600

    banner section finished

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navigation from '../Navigation/Navigation';
+import Banner from '../Banner/Banner';
 
 const Header = () => {
     return (
         <div>
             <Navigation></Navigation>
+            <Banner></Banner>
         </div>
     );
 };
