8182713a29d81bde310111b6afefaa7a1cae13eb
Hisham6667 <hisham.44678@gmail.com>
2023-05-03 12:00:37 +0600

    route created for allchef component and data loaded

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -5,6 +5,7 @@ import Home from '../components/Home/Home';
 import Login from '../components/Login/Login';
 import Register from '../components/Register/Register';
 import { createBrowserRouter } from 'react-router-dom';
+import AllChef from '../components/AllChef/AllChef';
 
 const router = createBrowserRouter([
     {
@@ -13,7 +14,14 @@ const router = createBrowserRouter([
         children: [
             {
                 path: '/',
-                element: <Home></Home>
+                element: <Home></Home>,
+                children: [
+                    {
+                        path: '/chefs',
+                        element: <AllChef></AllChef>,
+                        loader: () => fetch('http://localhost:5000/allchef')
+                    }
+                ]
             },
         ]
     },
---
+++ src/components/AllChef/AllChef.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllChef = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AllChef;
